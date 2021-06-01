3e2128af8aea62248a0b74d7ceaa395e0d8ae12f
Sargis Hovsepyan <shovsepy@c1r9s4.42yerevan.am>
2021-06-01 13:21:30 +0400

    ft_server

---
+++ srcs/setup.sh
@@ -0,0 +1,31 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    setup.sh                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/05/27 17:41:54 by shovsepy          #+#    #+#              #
+#    Updated: 2021/05/27 17:41:57 by shovsepy         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!/bin/bash
+service nginx start
+service mysql start
+service php7.3-fpm start
+
+db_name='wp_base'
+username='admin'
+password='admin'
+hostname='localhost'
+
+mysql -e "CREATE DATABASE $db_name;"
+mysql -e "CREATE USER '$username'@'localhost' IDENTIFIED BY '$password';"
+mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$username'@'$hostname' WITH GRANT OPTION;"
+mysql -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE user='$username';"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysql < /var/www/server/phpmyadmin/sql/create_tables.sql
+
+bash
